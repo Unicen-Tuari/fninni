f716ed45169d714d518e62cb03c8f560dd8c843a
facuninni <facuninni@gmail.com>
2015-06-10 01:57:20 -0300

    probando dados para entregar

---
+++ Dados A entregar/js/cubilete.js
@@ -0,0 +1,52 @@
+"use strict";
+
+var tiradas = [0,0,0,0,0,0,0,0,0,0,0];
+var repetidas=[0,0,0,0,0,0];
+var timer;
+
+function lanzarUnDado(idDado) {
+  var dado = document.getElementById(idDado);
+  var num = Math.floor(Math.random()*7)+1;
+  //alert("valor  "+num);
+  dado.src = "images/dado"+num+".png";
+  if (num==7){
+    imprimirRepetidas(1);
+    return 1}
+  else
+      {imprimirRepetidas(num);
+        return num;}
+
+}
+
+function imprimir(suma){
+  var resultado = document.getElementById(suma);
+  resultado.innerHTML = tiradas[suma-2];
+}
+function imprimirRepetidas(num){
+  var repe = document.getElementById("c"+num);
+  repetidas[num-1]++;
+  repe.innerHTML = repetidas[num-1];
+}
+
+function lanzarDados()
+{
+  var sumaux=0;
+  for (var i = 0; i < document.getElementById('veces').value; i++) {
+    var suma = lanzarUnDado("dado1") + lanzarUnDado("dado2");
+    //alert("valor de suma es "+suma);
+    if (suma >= sumaux){
+      sumaux=suma;
+      }
+    tiradas[suma-2]++;
+    imprimir(suma);
+  }
+  //alert(sumaux);
+}
+
+function agregarTarea(){
+  var lista = document.getElementById('tareas');
+  var nuevaTarea = document.getElementById('tarea').value;
+  var item = document.createElement('li');
+  item.innerHTML = nuevaTarea;
+  lista.appendChild(item);
+}
